4eb1b8e1301f5f0e3f22d39dc3ce5b2bb02e7fde
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-09-04 10:54:36 -0700

    problem 3 done

--- problem3.java
+++ problem3.java
@@ -1,1 +1,16 @@
 
+public class problem3
+{
+  public static void main(String[] args)
+  {
+    String printMsg1 = "=========";
+    String printMsg2 = "= 0   0 =   Java is Awesome!";
+    String printMsg3 = "=   v   =";
+    String printMsg4 = "=========";
+    System.out.println(printMsg1);
+    System.out.println(printMsg2);
+    System.out.println(printMsg3);
+    System.out.println(printMsg4);
+  }
+}
+
