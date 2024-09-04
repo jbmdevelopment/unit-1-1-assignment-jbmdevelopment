b168b62a8c97bae7620682cf943d0144124b360e
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-09-04 10:41:40 -0700

    problem 1 committed

--- problem1.java
+++ problem1.java
@@ -1,1 +1,8 @@
-
+public class problem1
+{
+  public static void main(String[] args)
+  {
+    String favSportsPlayer = "My favorite sports player is Lionel Messi";
+    System.out.print("        " + favSportsPlayer + "        ");
+  }
+}
