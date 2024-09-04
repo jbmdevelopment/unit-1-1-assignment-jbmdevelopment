ce8e93792010183ddc963b005ea1ae5442b8fb86
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-09-04 10:50:43 -0700

    problem2 done

--- problem2.java
+++ problem2.java
@@ -1,1 +1,11 @@
+public class problem2
+{
+  public static void main(String[] args)
+  {
+    String printMsg = "Coding is fun!";
+    System.out.println(printMsg);
+    System.out.println(printMsg);
+    System.out.println(printMsg);
+  }
+}
 
